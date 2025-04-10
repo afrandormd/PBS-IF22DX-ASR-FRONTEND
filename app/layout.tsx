5f43c4ad38c567d4f46c2025eadf665a8cc5cecb
afrandormd <afrandormd@gmail.com>
2025-04-10 19:55:07 +0700

    feat: menambahkan logo UTI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="synthwave">
+    <html lang="en" data-theme="cupcake">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* area header */}
+        <header>
+          <Image width={320} height={60} src={"/images/logo-teknokrat.png"} alt="gambar logo teknokrat" />
+        </header>
+
+        {/* area content */}
         {children}
+
+        {/* area footer */}
+        <footer>
+          Copyright &copy; 2025 - IF 22 DX
+        </footer>
       </body>
     </html>
   );
