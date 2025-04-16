b9edadbe6c706dbd8439244b713a70d5c663d7b6
afrandormd <afrandormd@gmail.com>
2025-04-16 20:34:42 +0700

    chore: fetch API dengan SWR

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+'use client'
+import useSWR from "swr";
+
+// buat variable fetcher
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function Home() {
+  // buat variable untuk SWR
+const { data, error, isLoading } = useSWR(
+    "http://localhost:3001/api/user",
+    fetcher
+  );
+
   return (
     <div>
       {/* buat tombol Tambah Data */}
@@ -20,12 +32,18 @@ export default function Home() {
           </thead>
           <tbody>
             {/* row */}
+            {/* mulai looping "map" */}
+            {data?.data_user.map((item: any) => (
             <tr className="hover:bg-teal-100">
-              <td className="text-center">2</td>
-              <td className="text-left">Hart Hagerty</td>
-              <td className="text-center">Desktop Support Technician</td>
-              <td className="text-center">Purple</td>
+              <td className="text-center">-</td>
+              <td className="text-left">{item.nama}</td>
+              <td className="text-center">{item.username}</td>
+              <td className="text-center">{item.password}</td>
             </tr>
+            ))}
+
+
+          {/* akhir looping "map" */}
           </tbody>
         </table>
       </section>
